c829b02cf6c462221e541c87268e319710899742
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-09-01 20:34:35 -0300

    Tipos de parâmentros  *  * Route Params => Identificar um recurso editar/deletar/buscar  * Query Params => Paginação / Filtro * Body Params => Os Objetos inserção/ alteração(JSON)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
 /**
  * Utilizando os métodos HTTP 
  * GET - Buscar uma informação dentro do servidor
@@ -8,14 +10,27 @@ const app = express();
  * PUT - Alterar uma informação no servidor
  * PATCH - Alterar uma informoção especifica
  * DELETE - Deletar uma informação do servidor
+ * 
+ * 
+ * Tipos de parâmentros
+ * 
+ * Route Params => Identificar um recurso editar/deletar/buscar
+ * Query Params => Paginação / Filtro
+ * Body Params => Os Objetos inserção/ alteração(JSON)
  */
 app.get("/courses",(request,response)=>{
+    const query = request.query;
+    console.log(query);
     return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
 });
 app.post("/courses",(request, response)=>{
+    const body = request.body;
+    console.log(body);
     return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
 });
 app.put("/courses/:id",(request,response) =>{
+    const {id} = request.params;
+    console.log(id);
     return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
 
 });
