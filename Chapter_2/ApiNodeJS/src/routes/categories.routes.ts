e9cfddca154530008ab66a7877f3a0c4ccd7a08f
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-09-19 10:18:16 -0300

    Inserindo tipagem para categoria

--- Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
+++ Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
@@ -1,17 +1,25 @@
 import { Router } from "express";
 import { v4 as uuidV4 } from "uuid";
 
+import { Category } from "../model/Category";
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  const category = { id: uuidV4(), name, description, created_at: new Date() };
+
+  const category = new Category();
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json(category);
 });
 
 export { categoriesRoutes };
