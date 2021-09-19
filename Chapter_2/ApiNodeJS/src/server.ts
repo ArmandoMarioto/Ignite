fae1f50f29c54b10f7ffd6b3fe84b9cad686c6b7
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-09-19 09:43:21 -0300

    Diagrama de classe e rota de criado categoria feita.

--- Chapter_2/ApiNodeJS/src/server.ts
+++ Chapter_2/ApiNodeJS/src/server.ts
@@ -1,12 +1,10 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 app.use(express.json());
 
-app.get("/", (request, response) => response.json({ message: "Hello world!" }));
+app.use(categoriesRoutes);
 
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
 app.listen(3333, () => console.log("Server listening on"));
