3aa2bac77659b93a133d83a38c04439df6fd3a98
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-09-19 10:03:30 -0300

    Inserindo ID com uuid na categoria

--- Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
+++ Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  categories.push({ name, description });
+  const category = { id: uuidV4(), name, description, created_at: new Date() };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
