e12e7e3f4182ae3d9d348b8a112f980dfe75e270
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-09-21 20:49:17 -0300

    Utilizando o Princípio da Substituição de Liskov (LSP)

--- Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
+++ Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { PostgresCategoriesRepository } from "../repositories/PostgresCategoriesRepository";
 import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = new PostgresCategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
--- Chapter_2/ApiNodeJS/src/services/CreateCategoryService.ts
+++ Chapter_2/ApiNodeJS/src/services/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
@@ -11,9 +11,9 @@ interface IRequest {
  */
 
 class CreateCategoryService {
-  constructor(private categoriesRepository: CategoriesRepository) {}
+  constructor(private categoriesRepository: ICategoriesRepository) {}
   execute({ description, name }: IRequest): void {
-    const categoryAlreadyExists = this.categoriesRepository.findbyName(name);
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists) {
       throw new Error(`Category ${name} already exists`);
