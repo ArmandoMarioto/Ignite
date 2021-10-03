0d2295ca0f32d4daf6140490bc9a73ae6267c113
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-03 11:59:39 -0300

    Refatorando a listagem de categoria

--- Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
+++ Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
@@ -2,17 +2,16 @@ import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-  return response.status(200).json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
