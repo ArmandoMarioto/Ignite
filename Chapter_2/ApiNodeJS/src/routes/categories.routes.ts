eff3a8ac8bd1e5fdedf0b0473a07caf819449329
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-03 12:12:45 -0300

    Separando os repositórios

--- Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
+++ Chapter_2/ApiNodeJS/src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
