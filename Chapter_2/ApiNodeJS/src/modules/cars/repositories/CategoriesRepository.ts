558972bb8a496aeb1bbac3a6706f29ab4e4cde4e
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-03 12:06:39 -0300

    Conhecendo Singleton Pattern

--- Chapter_2/ApiNodeJS/src/modules/cars/repositories/CategoriesRepository.ts
+++ Chapter_2/ApiNodeJS/src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,11 +1,21 @@
 import { Category } from "../model/Category";
 import { ICreateCategoryDTO } from "./ICategoriesRepository";
 
+// singleton
 class CategoriesRepository {
   private categories: Category[] = [];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
