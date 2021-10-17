90f0d9b0759371833a0e80c5b27bfb34dc20b6ee
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-17 09:00:16 -0300

    Configurando o swagger

--- Chapter_2/ApiNodeJS/src/server.ts
+++ Chapter_2/ApiNodeJS/src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFiles from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFiles));
 
 app.use(router);
 
