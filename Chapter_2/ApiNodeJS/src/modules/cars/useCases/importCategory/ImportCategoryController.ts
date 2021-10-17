d5e1fc6fccc654a9c71335a06e72f3dca256799c
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-16 21:57:52 -0300

    Conhecendo o conceito de stream

--- Chapter_2/ApiNodeJS/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ Chapter_2/ApiNodeJS/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from "express";
+
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
--- Chapter_2/ApiNodeJS/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Chapter_2/ApiNodeJS/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,15 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+    const parsefile = csvParse();
+    stream.pipe(parsefile);
+
+    parsefile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
