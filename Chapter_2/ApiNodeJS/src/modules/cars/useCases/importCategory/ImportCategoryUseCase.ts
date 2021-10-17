ee201c620b9b97a397c9417675408fdc450e3247
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-17 09:22:30 -0300

    Removendo os arquivos de upload

--- Chapter_2/ApiNodeJS/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Chapter_2/ApiNodeJS/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -27,6 +27,7 @@ class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
